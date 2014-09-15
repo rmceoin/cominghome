af494e2d5da4717abdcbc8876b44a70fc168a354
rmceoin <1653739+rmceoin@users.noreply.github.com>
2014-09-15 16:26:11 -0700

    Check away status from Nest before setting it

--- backend/src/main/java/net/mceoin/cominghome/backend/MyServlet.java
+++ backend/src/main/java/net/mceoin/cominghome/backend/MyServlet.java
@@ -133,7 +133,13 @@ public class MyServlet extends HttpServlet {
                         }
                     }
                     if (doit) {
-                        nest_result = tellNestAwayStatus(access_token, structure_id, away_status);
+                        String nest_away_status = getNestAwayStatus(access_token);
+                        if (!nest_away_status.equals(away_status)) {
+                            nest_result = tellNestAwayStatus(access_token, structure_id, away_status);
+                        } else {
+                            log.info("Nest already set to "+nest_away_status);
+                            nest_result = "Already "+away_status;
+                        }
                     }
                 }
             }
@@ -325,5 +331,133 @@ public class MyServlet extends HttpServlet {
         } else {
             return "Success";
         }
+    }
+
+    private String getNestAwayStatus(String access_token) {
+
+        String away_status="";
+
+        String urlString = "https://developer-api.nest.com/structures?auth=" + access_token;
+        log.info("url=" + urlString);
+
+        StringBuilder builder = new StringBuilder();
+        boolean error = false;
+        String errorResult = "";
+
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("User-Agent", "ComingHomeBackend/1.0");
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+//            urlConnection.setChunkedStreamingMode(0);
+
+//            urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf8");
+
+            boolean redirect = false;
+
+            // normally, 3xx is redirect
+            int status = urlConnection.getResponseCode();
+            if (status != HttpURLConnection.HTTP_OK) {
+                if (status == HttpURLConnection.HTTP_MOVED_TEMP
+                        || status == HttpURLConnection.HTTP_MOVED_PERM
+                        || status == 307    // Temporary redirect
+                        || status == HttpURLConnection.HTTP_SEE_OTHER)
+                    redirect = true;
+            }
+
+//            System.out.println("Response Code ... " + status);
+
+            if (redirect) {
+
+                // get redirect url from "location" header field
+                String newUrl = urlConnection.getHeaderField("Location");
+
+                // open the new connnection again
+                urlConnection = (HttpURLConnection) new URL(newUrl).openConnection();
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+//                urlConnection.setChunkedStreamingMode(0);
+
+//                System.out.println("Redirect to URL : " + newUrl);
+
+            }
+
+            int statusCode = urlConnection.getResponseCode();
+
+            log.info("statusCode=" + statusCode);
+            if ((statusCode == HttpURLConnection.HTTP_OK)) {
+                error = false;
+
+                InputStream response;
+                response = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+                log.info("response=" + builder.toString());
+                JSONObject object = new JSONObject(builder.toString());
+
+                Iterator keys = object.keys();
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    JSONObject structure = object.getJSONObject(key);
+
+                    if (structure.has("away")) {
+                        away_status = structure.getString("away");
+                    } else {
+                        log.info("missing away");
+                    }
+                }
+
+            } else if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                // bad auth
+                error = true;
+                errorResult = "Unauthorized";
+            } else if (statusCode == HttpURLConnection.HTTP_BAD_REQUEST) {
+                error = true;
+                InputStream response;
+                response = urlConnection.getErrorStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+                log.info("response=" + builder.toString());
+                JSONObject object = new JSONObject(builder.toString());
+
+                Iterator keys = object.keys();
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    if (key.equals("error")) {
+                        // error = Internal Error on bad structure_id
+                        errorResult = object.getString("error");
+                        log.info("errorResult=" + errorResult);
+                    }
+                }
+            } else {
+                error = true;
+                errorResult = Integer.toString(statusCode);
+            }
+
+        } catch (IOException e) {
+            errorResult = e.getLocalizedMessage();
+            log.warning("IOException: " + errorResult);
+        } catch (Exception e) {
+            errorResult = e.getLocalizedMessage();
+            log.warning("Exception: " + errorResult);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+
+       return away_status;
     }
 }
