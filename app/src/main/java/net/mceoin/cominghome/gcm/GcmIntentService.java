ac3e91fb2c61f9fd8ce43807d47580993ca09820
rmceoin <rmceoin@gmail.com>
2015-02-03 07:13:12 -0800

    Starts up LocationServices, gets location, if at home, let the backend know

--- app/src/main/java/net/mceoin/cominghome/gcm/GcmIntentService.java
+++ app/src/main/java/net/mceoin/cominghome/gcm/GcmIntentService.java
@@ -2,19 +2,39 @@ package net.mceoin.cominghome.gcm;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.location.LocationServices;
+
+import net.mceoin.cominghome.LocationUtils;
+import net.mceoin.cominghome.MainActivity;
+import net.mceoin.cominghome.geofence.FenceHandling;
+import net.mceoin.cominghome.geofence.SimpleGeofence;
+import net.mceoin.cominghome.geofence.SimpleGeofenceStore;
 
 /**
  * Handle a GCM message from the backend
  * <p/>
  * Inspired from https://developer.android.com/google/gcm/client.html
  */
-public class GcmIntentService extends IntentService {
+public class GcmIntentService extends IntentService implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    
     private static final String TAG = GcmIntentService.class.getSimpleName();
     private static final boolean debug = true;
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    private SimpleGeofenceStore mGeofenceStorage;
 
     public GcmIntentService() {
         super("GcmIntentService");
@@ -55,18 +75,85 @@ public class GcmIntentService extends IntentService {
     }
 
     protected void processMessage(Bundle extras) {
-        String command = extras.getString( "command", "none");
-        String message = extras.getString( "message", "none");
-     
-        if (debug) Log.d(TAG, "command=" + command + " message="+ message);
+        String command = extras.getString("command", "none");
+        String message = extras.getString("message", "none");
+
+        if (debug) Log.d(TAG, "command=" + command + " message=" + message);
         switch (command) {
             case "check-in":
                 if (debug) Log.d(TAG, "need to check in");
+                checkIn();
                 break;
             case "none":
             default:
                 Log.w(TAG, "unknown command: " + command);
                 break;
         }
+    }
+
+    protected void checkIn() {
+        if (mGoogleApiClient==null) {
+            buildGoogleApiClient();
+        }
+        if (mGoogleApiClient!=null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is trying to connect but failed.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is connected.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (debug) Log.d(TAG, "GoogleApiClient connected");
+        Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mCurrentLocation == null) {
+            return;
+        }
+        if (debug) Log.d(TAG, mCurrentLocation.toString());
+        double latitude = mCurrentLocation.getLatitude();
+        double longitude = mCurrentLocation.getLongitude();
+        
+        mGoogleApiClient.disconnect();
+
+        mGeofenceStorage = new SimpleGeofenceStore(getApplicationContext());
+        SimpleGeofence homeGeofence = mGeofenceStorage.getGeofence(MainActivity.FENCE_HOME);
+
+        if (homeGeofence != null) {
+            float distFromHome = LocationUtils.distFrom(latitude, longitude,
+                    homeGeofence.getLatitude(), homeGeofence.getLongitude());
+            if (distFromHome <= homeGeofence.getRadius()) {
+                if (debug) Log.d(TAG, "we're at home");
+                FenceHandling.arrivedHome(getApplicationContext());
+            }
+        }
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} connection is suspended.
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
     }
 }
