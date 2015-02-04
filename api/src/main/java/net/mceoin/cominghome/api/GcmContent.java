9553439689355afc987f73e7c07aac6e4454842d
rmceoin <rmceoin@gmail.com>
2015-02-03 17:49:27 -0800

    Added TTL and collapse

--- api/src/main/java/net/mceoin/cominghome/api/GcmContent.java
+++ api/src/main/java/net/mceoin/cominghome/api/GcmContent.java
@@ -13,19 +13,40 @@ public class GcmContent implements Serializable {
 
     private List<String> registration_ids;
     private Map<String, String> data;
+    private int time_to_live;
+    private String collapse_key;
 
     public List<String> getRegistration_ids() {
         return registration_ids;
     }
-    
+
     public Map<String, String> getData() {
         return data;
     }
-    
+
+    public int getTime_to_live() {
+        return time_to_live;
+    }
+
+    public void setTime_to_live(int time_to_live) {
+        this.time_to_live = time_to_live;
+    }
+
+    public String getCollapse_key() {
+        return collapse_key;
+    }
+
+    public void setCollapse_key(String collapse_key) {
+        this.collapse_key = collapse_key;
+    }
+
     public void addRegId(String regId) {
         if (registration_ids == null)
             registration_ids = new LinkedList<String>();
-        registration_ids.add(regId);
+        if (!registration_ids.contains(regId)) {
+            // don't allow duplicates
+            registration_ids.add(regId);
+        }
     }
 
     public void createData(String command, String message) {
