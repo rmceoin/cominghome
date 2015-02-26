c1a798f8f0b2ad42a2739aaab5e02ffe9526f28a
rmceoin <rmceoin@gmail.com>
2015-02-25 17:51:52 -0800

    Switched from using bundle to shared preferences to track start time

--- app/src/main/java/net/mceoin/cominghome/geofence/FenceHandlingAlarm.java
+++ app/src/main/java/net/mceoin/cominghome/geofence/FenceHandlingAlarm.java
@@ -21,7 +21,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -36,20 +36,31 @@ public class FenceHandlingAlarm extends BroadcastReceiver {
     public final static String TAG = FenceHandlingAlarm.class.getSimpleName();
     public final static boolean debug = false;
 
-    protected long startTime;
+    /**
+     * Preference for saving the start time of the alarm.
+     */
+    private final String PREF_STARTTIME = "alarm_start_time";
+
+    /**
+     * Name of dedicated preferences file.
+     */
+    private final String PREF_NAME = "alarm_preferences";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (debug) Log.d(TAG, "onReceive()");
 
         long currentTime = System.currentTimeMillis();
-        long alarmStartTime = intent.getLongExtra("start", startTime);
+        long alarmStartTime = getStartTime(context);
+
         long timeElapsedSeconds = (currentTime - alarmStartTime) / 1000;
         if (debug)
             Log.d(TAG, "extra alarmStartTime=" + alarmStartTime + " timeElapsedSeconds=" + timeElapsedSeconds);
-        if (timeElapsedSeconds < (10 * 60)) {
+
+        int minimumMinutes = 10;
+        if (timeElapsedSeconds < (minimumMinutes * 60)) {
             if (debug)
-                Log.d(TAG, "not enough time has passed: " + timeElapsedSeconds + " = " + currentTime + " - " + alarmStartTime + "/1000");
+                Log.d(TAG, "not enough time has passed: (" + timeElapsedSeconds + " = " + currentTime + " - " + alarmStartTime + ")/1000");
             return;
         }
 
@@ -58,17 +69,19 @@ public class FenceHandlingAlarm extends BroadcastReceiver {
         CancelAlarm(context);
     }
 
+    /**
+     * Set an alarm to wait before we actually send the backend a left home status update.
+     *
+     * @param context Context
+     */
     public void SetAlarm(@NonNull Context context) {
         if (debug) Log.d(TAG, "SetAlarm()");
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, FenceHandlingAlarm.class);
 
         long alarmStartTime = System.currentTimeMillis();
-        startTime = alarmStartTime;
+        saveStartTime(context, alarmStartTime);
 
-        Bundle mBundle = new Bundle();
-        mBundle.putLong("start", alarmStartTime);
-        i.putExtras(mBundle);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
         if (debug) Log.d(TAG, "alarmStartTime=" + alarmStartTime);
 
@@ -83,5 +96,34 @@ public class FenceHandlingAlarm extends BroadcastReceiver {
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(sender);
+    }
+
+    /**
+     * Saves the start time of the alarm in a MODE_MULTI_PROCESS preference which ensures
+     * that different processes will put and get the current value.
+     *
+     * @param context   Context
+     * @param startTime Start time in epoch
+     */
+    private void saveStartTime(@NonNull Context context, long startTime) {
+        SharedPreferences prefs;
+        prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_MULTI_PROCESS);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong(PREF_STARTTIME, startTime);
+        editor.apply();
+
+    }
+
+    /**
+     * Get the start time of the alarm in epoch.
+     *
+     * @param context Context
+     * @return Time in epoch
+     */
+    private long getStartTime(@NonNull Context context) {
+        SharedPreferences prefs;
+        prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_MULTI_PROCESS);
+        return prefs.getLong(PREF_STARTTIME, 0);
+
     }
 }
