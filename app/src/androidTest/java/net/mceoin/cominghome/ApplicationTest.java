7f5864daa395945a78c4a4b214aff378a2ccc6f7
rmceoin <1653739+rmceoin@users.noreply.github.com>
2015-03-02 17:47:47 -0800

    First basic test

--- app/src/androidTest/java/net/mceoin/cominghome/ApplicationTest.java
+++ app/src/androidTest/java/net/mceoin/cominghome/ApplicationTest.java
@@ -1,10 +1,25 @@
 package net.mceoin.cominghome;
 
 import android.app.Application;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
 
+/**
+ * Under 'Settings->Developer options' disable the following 3 settings and restart the device:
+ * o Window Animation Scale
+ * o Transition Animation Scale
+ * o Animator Duration Scale
+ */
 @LargeTest
 public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -18,8 +33,11 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
         getActivity();
     }
 
-    public void testListGoesOverTheFold() {
-        //TODO figure out how to get Espresso working
-//        onView(withText("Hello world")).check(isDisplayed());
+    public void testConnectToNest() {
+        final String STRING_CONNECT_TO_NEST = "Connect to Nest";
+        
+        onView(withId(R.id.buttonConnectNest)).check(matches(withText(STRING_CONNECT_TO_NEST)));
+
+        onView(withId(R.id.buttonSetAtHome)).perform(click());
     }
 }
