6d8b700ce96b266e71cd8ac40e5a0ec0b9505476
rmceoin <rmceoin@gmail.com>
2015-12-05 16:03:36 -0800

    Remove unused contants

--- app/src/main/java/net/mceoin/cominghome/geofence/GeofenceUtils.java
+++ app/src/main/java/net/mceoin/cominghome/geofence/GeofenceUtils.java
@@ -21,51 +21,6 @@ package net.mceoin.cominghome.geofence;
  */
 public final class GeofenceUtils {
 
-    // Used to track what type of geofence removal request was made.
-//    public enum REMOVE_TYPE {INTENT, LIST}
-
-    // Used to track what type of request is in process
-//    public enum REQUEST_TYPE {ADD, REMOVE}
-
-    // Intent actions
-//    public static final String ACTION_CONNECTION_ERROR =
-//            "com.example.android.geofence.ACTION_CONNECTION_ERROR";
-
-//    public static final String ACTION_CONNECTION_SUCCESS =
-//            "com.example.android.geofence.ACTION_CONNECTION_SUCCESS";
-
-//    public static final String ACTION_GEOFENCES_ADDED =
-//            "com.example.android.geofence.ACTION_GEOFENCES_ADDED";
-
-//    public static final String ACTION_GEOFENCES_REMOVED =
-//            "com.example.android.geofence.ACTION_GEOFENCES_DELETED";
-
-//    public static final String ACTION_GEOFENCE_ERROR =
-//            "com.example.android.geofence.ACTION_GEOFENCES_ERROR";
-
-//    public static final String ACTION_GEOFENCE_TRANSITION =
-//            "com.example.android.geofence.ACTION_GEOFENCE_TRANSITION";
-//
-//    public static final String ACTION_GEOFENCE_TRANSITION_ERROR =
-//                    "com.example.android.geofence.ACTION_GEOFENCE_TRANSITION_ERROR";
-
-    // The Intent category used by all Location Services sample apps
-//    public static final String CATEGORY_LOCATION_SERVICES =
-//                    "com.example.android.geofence.CATEGORY_LOCATION_SERVICES";
-
-    // Keys for extended data in Intents
-//    public static final String EXTRA_CONNECTION_CODE =
-//                    "com.example.android.EXTRA_CONNECTION_CODE";
-
-//    public static final String EXTRA_CONNECTION_ERROR_CODE =
-//            "com.example.android.geofence.EXTRA_CONNECTION_ERROR_CODE";
-
-//    public static final String EXTRA_CONNECTION_ERROR_MESSAGE =
-//            "com.example.android.geofence.EXTRA_CONNECTION_ERROR_MESSAGE";
-
-//    public static final String EXTRA_GEOFENCE_STATUS =
-//            "com.example.android.geofence.EXTRA_GEOFENCE_STATUS";
-
     /*
      * Keys for flattened geofences stored in SharedPreferences
      */
@@ -92,28 +47,5 @@ public final class GeofenceUtils {
 
     public static final int INVALID_INT_VALUE = -999;
 
-    /*
-     * Constants used in verifying the correctness of input values
-     */
-//    public static final double MAX_LATITUDE = 90.d;
-//
-//    public static final double MIN_LATITUDE = -90.d;
-//
-//    public static final double MAX_LONGITUDE = 180.d;
-//
-//    public static final double MIN_LONGITUDE = -180.d;
-//
-//    public static final float MIN_RADIUS = 1f;
-
-    /*
-     * Define a request code to send to Google Play services
-     * This code is returned in Activity.onActivityResult
-     */
-//    public final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
-//    // A string of length 0, used to clear out input fields
-//    public static final String EMPTY_STRING = new String();
-
-//    public static final CharSequence GEOFENCE_ID_DELIMITER = ",";
 
 }
